e7799a87071ee06c21e3691cf67aa985f0996924
YoungBeans <klog123480@gmail.com>
2019-11-13 19:05:43 +0900

    main page modify

--- reservation/reservation/settings.py
+++ reservation/reservation/settings.py
@@ -122,5 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main','static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
